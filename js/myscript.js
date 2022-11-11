ce389bce0ac85b22a04d3aed3954dfe297e7de89
Paulo <psfaraujo@gmail.com>
2022-11-10 16:08:04 -0800

    V.1.1 - Not showing fingers images, synchronous execution not working

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,38 @@
+function evenOdds() {
 var evens = prompt("Input EVENS player's name");
+document.getElementById("evens").innerHTML = evens;
 var odds = prompt("Input ODDS player's name");
+document.getElementById("odds").innerHTML = odds;
+const next = runGame()
+return next;
 
-function evenOdds () {
-var randomP1 = Math.floor(Math.random() * 5);
-randomP1 = randomP1+1;
-var randomP2 = Math.floor(Math.random() * 5);
-randomP2 = randomP2+1;
-var result = randomP1 + randomP2;
-console.log(randomP1, randomP2, result);
-return result;
+function runGame() {
+    var randomP1 = Math.floor(Math.random() * 5);
+    randomP1 = randomP1 + 1;
+    var randomP2 = Math.floor(Math.random() * 5);
+    randomP2 = randomP2 + 1;
+    var result = randomP1 + randomP2;
+    console.log(randomP1, randomP2, result);
+
+    alert("Result is " + result);
+
+    if (result % 2 === 0) {
+        alert(evens + " wins")
+    } else {
+        alert(odds + " wins")
+    }
+
+    return result;
 }
-
-alert ("Result is "+ result);
-
-if (result % 2 === 0) {
-    alert(evens + " wins")
-} else {
-   alert(odds + " wins")
 }
+// alert(result);
+//function displayFingersL () {
 
-//evenOdds ()
+//}
+
+
+
+let btnPlay = document.getElementById("btnPlay");
+btnPlay.addEventListener('click', event => {
+    evenOdds();
+});
