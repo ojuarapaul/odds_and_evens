5297763cc436bb214b026faf3d63f0508e2a563b
Paulo <psfaraujo@gmail.com>
2022-11-16 12:43:56 -0800

    Comments added

--- js/myscript.js
+++ js/myscript.js
@@ -19,7 +19,7 @@ function evenOdds() {
     const next = runGame()
     return next;
 
-    function runGame() {
+    function runGame() { // chooses a random number from 1 to 5 then displays the correspondent hand image
         var randomP1 = Math.floor(Math.random() * 5);
         randomP1 = randomP1 + 1;
         var img = document.createElement("img");
@@ -37,10 +37,10 @@ function evenOdds() {
         img.style.width = '60%';
         img.style.height = 'auto';
         src.appendChild(img);
-
+        // add the two random numbers found
         var result = randomP1 + randomP2;
         document.getElementById("resultFinal").innerHTML = result;
-
+        // find out if the result it's an even or odd number and declare who wins
         if (result % 2 === 0) {
             var winner = evens + " WON!";
             var winner = winner.toUpperCase();
